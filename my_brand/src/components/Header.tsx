9b22bad2e4ea2fb97cbaa33660b8673ff379ce3b
Sng43 <daemmyoff1cial@gmail.com>
2024-08-26 16:39:26 +0200

    start

---
+++ my_brand/src/components/Header.tsx
@@ -0,0 +1,24 @@
+import Logo from "./Logo";
+
+const Header = () => {
+    return (
+        <header className='bg-dark sticky top-0 z-[20] mx-auto flex w-full p-0 text-gray-400 '>
+            <div className="flex m-auto w-[94%] items-center justify-between">
+        <Logo/>
+        <div className="hidden sm:flex border-solid border w-[60%]">
+            <ul className="flex justify-between items-center w-full">
+            <li>SERVICES</li>
+            <li>WORKS</li>
+            <li>BLOGS</li>
+            <li>ABOUT</li>
+            <li>CONTACT</li>
+            </ul></div>
+        <div className="sm:hidden ">
+            <p className="hover:text-white">burger</p>
+        </div>
+                </div> 
+        </header>
+    );
+}
+
+export default Header;
---
+++ my_brand/src/pages/HomePage.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const HomePage = () => {
+    return (
+        <>
+    <Header/>
+    <div className='w-[94%] m-auto'>
+        <h1 className=''>SENGA EMMY</h1>
+    </div>
+    <div>Services</div>
+    <div>Work</div>
+    <div>About</div>
+    <div>Contact</div>
+    <div>footer</div>
+        </>
+    );
+}
+
+export default HomePage;
