66a3546aba90c70be6c9e6ba9187b52c6415a98f
Sng43 <daemmyoff1cial@gmail.com>
2024-08-29 03:39:05 +0200

    flex-row

--- my_brand/src/Sections/Services.tsx
+++ my_brand/src/Sections/Services.tsx
@@ -19,13 +19,13 @@ const Services = () => {
                     <p className="sm:w-[45%] mt-6 lg:mt-5 font-antic leading-[23px] text-black tracking-normal text-lg lg:text-xl lg:leading-7 lg:tracking-wider">My obsession is to craft memorable digital experiences that represent your brand, serve a meaningful purpose, and drive tangible business results.</p>
                     <ul className="sm:w-[55%] sm:ml-28">
                         <li className="sm:ml-10 border-b-2 border-black">
-                        <h1 className="sm:ml-4 font-antic font-extrabold text-2xl tracking-tightest mb-2 mt-4"><span className="font-oxanium-thin font-light text-xl text-gray-600 mx-2" >01</span>Responsive Design</h1> 
+                        <h1 className="sm:ml-4 lg:text-4xl font-antic font-extrabold text-2xl tracking-tightest mb-2 mt-4"><span className="font-oxanium-thin font-light text-xl text-gray-600 mx-2" >01</span>Responsive Design</h1> 
                         </li>
                         <li className="sm:ml-10 border-b-2 border-black">
-                        <h1 className="sm:ml-4 font-antic font-extrabold text-2xl tracking-tightest my-2"><span className="font-oxanium-thin font-light text-xl text-gray-600 mx-2" >02</span>Responsive Design</h1>  
+                        <h1 className="sm:ml-4 lg:text-4xl font-antic font-extrabold text-2xl tracking-tightest my-2"><span className="font-oxanium-thin font-light text-xl text-gray-600 mx-2" >02</span>Wireframing</h1>  
                         </li>
                         <li className="sm:ml-10">
-                        <h1 className="sm:ml-4 font-antic font-extrabold text-2xl tracking-tightest my-2"><span className="font-oxanium-thin font-light text-xl text-gray-600 mx-2" >03</span>Responsive Design</h1>
+                        <h1 className="sm:ml-4 lg:text-4xl font-antic font-extrabold text-2xl tracking-tightest my-2"><span className="font-oxanium-thin font-light text-xl text-gray-600 mx-2" >03</span>UX Writing</h1>
                         </li>
                     </ul> 
                 </div> 
--- my_brand/src/Sections/Work.tsx
+++ my_brand/src/Sections/Work.tsx
@@ -3,34 +3,34 @@ const Work = () => {
         <section className="w-[94%] sm:w-[90%] m-auto">
             <h1 className="font-antic font-extrabold text-[2.8rem] sm:text-6xl lg:text-8xl tracking-tight my-6 lg:my-10">MY WORKS/</h1>
             <div className="my-16 sm:flex justify-between items-center">
-                <div className="h-64 w-80 lg:h-80 lg:w-[400px] xl:w-[540px] bg-gray-400">
+                <div className="h-64 w-72 lg:h-80 lg:w-[400px] xl:w-[540px] bg-gray-400">
                 </div>
                 <div className="sm:w-[40%] ">
                 <h3 className="my-4 font-antic text-2xl xl:text-4xl tracking-widest">ATLP e-commerce</h3>
                 <p className="font-antic text-gray-400 hover:text-white text-lg xl:text-xl">My obsession is to craft memorable digital experiences that represent your brand, serve a meaningful purpose, and drive tangible business results.</p>
                 </div>
             </div>
-            <div className="my-16 sm:flex justify-between items-center">
-                <div className="sm:w-[40%] ">
-                <h3 className="my-4 font-antic text-2xl xl:text-4xl tracking-widest">ATLP e-commerce</h3>
-                <p className="font-antic text-gray-400 hover:text-white text-lg xl:text-xl">My obsession is to craft memorable digital experiences that represent your brand, serve a meaningful purpose, and drive tangible business results.</p>
-                </div>
+            <div className="my-16 sm:flex justify-between items-center flex-row-reverse">
                 <div className="h-64 w-72 lg:h-80 lg:w-[400px] xl:w-[540px] bg-gray-400"></div>
-            </div>
-            <div className="my-16 sm:flex justify-between items-center">
-                <div className="h-64 w-80 lg:h-80 lg:w-[400px] xl:w-[540px] bg-gray-400">
-                </div>
                 <div className="sm:w-[40%] ">
                 <h3 className="my-4 font-antic text-2xl xl:text-4xl tracking-widest">ATLP e-commerce</h3>
                 <p className="font-antic text-gray-400 hover:text-white text-lg xl:text-xl">My obsession is to craft memorable digital experiences that represent your brand, serve a meaningful purpose, and drive tangible business results.</p>
                 </div>
             </div>
             <div className="my-16 sm:flex justify-between items-center">
+                <div className="h-64 w-72 lg:h-80 lg:w-[400px] xl:w-[540px] bg-gray-400">
+                </div>
                 <div className="sm:w-[40%] ">
                 <h3 className="my-4 font-antic text-2xl xl:text-4xl tracking-widest">ATLP e-commerce</h3>
                 <p className="font-antic text-gray-400 hover:text-white text-lg xl:text-xl">My obsession is to craft memorable digital experiences that represent your brand, serve a meaningful purpose, and drive tangible business results.</p>
                 </div>
+            </div>
+            <div className="my-16 sm:flex justify-between items-center flex-row-reverse">
                 <div className="h-64 w-72 lg:h-80 lg:w-[400px] xl:w-[540px] bg-gray-400"></div>
+                <div className="sm:w-[40%] ">
+                <h3 className="my-4 font-antic text-2xl xl:text-4xl tracking-widest">ATLP e-commerce</h3>
+                <p className="font-antic text-gray-400 hover:text-white text-lg xl:text-xl">My obsession is to craft memorable digital experiences that represent your brand, serve a meaningful purpose, and drive tangible business results.</p>
+                </div>
             </div>
         </section>
     );
