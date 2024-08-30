3b0888a0d80863d842e56ddce0d0fbafc701be43
Sng43 <daemmyoff1cial@gmail.com>
2024-08-30 15:57:43 +0200

    phase 1

--- my_brand/src/Sections/Footer.tsx
+++ my_brand/src/Sections/Footer.tsx
@@ -2,10 +2,10 @@ import Logo from "../components/Logo";
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-100 text-black lg:h-64 border border-black">
+        <footer className="bg-gray-100 text-black lg:h-64 border-black">
             <div className="sm:w-[90%] w-[94%] m-auto">
                 <div className="lg:flex justify-between">
-                <div className="my-2 lg:mb-0 border border-gray-100 lg:h-40 lg:w-[31%]">
+                <div className="my-2 border border-gray-100 lg:w-[31%]">
                     <h4 className="font-oxanium font-bold text-[10px] tracking-wider text-gray-500 my-2 lg:mb-8">SERVICES</h4>
                     <div className="font-antic font-[1000] leading-5 lg:leading-8 lg:tracking-widest lg:text-xl">
                         <h2>WEB DESIGN</h2>
@@ -13,16 +13,16 @@ const Footer = () => {
                         <h2>BACK-END DEVELOPMENT</h2>
                     </div>
                 </div>
-                <div className="my-2 border border-gray-100 lg:w-[31%] lg:h-[249px]">
+                <div className="my-2 border border-gray-100 lg:w-[31%] lg:h-[0.1px] xl:mr-20">
                     <h4 className="font-oxanium font-bold text-[10px] tracking-wider text-gray-500 mt-2 mb-1 lg:mb-8">SIGN UP FOR THE NEWSLETTER</h4>
                     <form className="font-oxanium font-extrabold text-[13px] tracking-wider " action="">
                         <label htmlFor="email">
-                            <input className="bg-slate-100 border border-gray-100 border-dashed border-b-black p-2 w-[100%] text-black focus:outline-none" type="email" placeholder="Enter your email address"/>
+                            <input className="bg-gray-100 border border-gray-100 border-dashed border-b-black p-2 w-[100%] text-black focus:outline-none" type="email" placeholder="Enter your email address"/>
                         </label>
                     </form>
-                    <p className="font-oxanium text-[6px] mt-6 lg:mt-[130px] lg:mb-[2px] text-black tracking-[0.15rem]">WEBSITE  BY  SENGA EMMANUEL KABARE</p>
+                    <p className="font-oxanium text-[6px] mt-6 lg:mt-[130px] text-black tracking-[0.15rem]">WEBSITE  BY  SENGA EMMANUEL KABARE</p>
                 </div>
-                <div className="border border-gray-100 lg:h-40 lg:w-[31%]">
+                <div className="border border-gray-100 lg:w-[31%]">
                     <h4 className="font-oxanium font-bold text-[10px] tracking-wider text-gray-500 my-2 lg:mt-4 lg:mb-8">SITEMAP</h4>
                     <p className="font-oxanium font-extrabold text-black text-[10px] border border-gray-100 border-dashed border-b-black my-1">Home</p>
                     <p className="font-oxanium font-extrabold text-black text-[10px] border border-gray-100 border-dashed border-b-black my-1">Services</p>
@@ -31,7 +31,7 @@ const Footer = () => {
                     <p className="font-oxanium font-extrabold text-black text-[10px] border border-gray-100 border-dashed border-b-black my-1">Contacts</p>
                 </div>
             </div>
-            <div className="m-auto w-[80%] lg:w-48 lg:h-24 lg:ml-0 text-center lg:relative bottom-28">
+            <div className="m-auto w-[80%] lg:w-48 lg:h-2 lg:ml-0 text-center lg:relative bottom-4">
                 <h1 className="font-oxanium text-xl relative top-5 lg:top-2" >2024</h1>
                 <div className=" h-40 w-40 m-auto lg:h-12 lg:w-16">
                     <Logo />
--- my_brand/src/components/Header.tsx
+++ my_brand/src/components/Header.tsx
@@ -18,13 +18,19 @@ const Header = () => {
     ]
     return (
         <header className='bg-dark-800 sticky top-0 z-[20] mx-auto flex w-full p-0 text-gray-400'>
-            <div className="sm:w-[90%] flex m-auto w-[94%] items-center justify-between">
+            <div className="sm:w-[90%] sm:flex m-auto w-[94%] justify-between items-center">
+        <div className="flex justify-between items-center sm:h-16 sm:w-16">
         <div className="h-16 w-16">
         <Logo/>
         </div>
-        <div className="sm:flex w-[60%]">
-            <ul className={`sm:flex sm:justify-between sm:items-center w-full font-oxanium-thin
-                ${isOpen ? 'flex': 'hidden'}`}>
+            <div className="sm:hidden mr-2">
+                <FontAwesomeIcon icon={isOpen? faX : faBars} className='h-7 w-8'
+                onClick={() => setIsOpen(!isOpen)}/>
+            </div>
+        </div>
+        <div className="sm:flex w-[60%] m-auto sm:m-0">
+            <ul className={`sm:flex sm:justify-between sm:items-center w-full font-oxanium-thin text-center leading-[3rem] mt-10 sm:mt-0
+                ${isOpen ? 'block h-svh': 'hidden'}`}>
                 {Links.map((Link) => (
             <li key={Link.name} className="hover:text-white">
                 <a href={Link.link}>{Link.name}</a>
@@ -32,10 +38,6 @@ const Header = () => {
                 ))}
 
             </ul></div>
-        <div className="sm:hidden mr-2">
-            <FontAwesomeIcon icon={isOpen? faX: faBars} className='h-7 w-8'
-            onClick={() => setIsOpen(!isOpen)}/>
-        </div>
                 </div> 
         </header>
     );
